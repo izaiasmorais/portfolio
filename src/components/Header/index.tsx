000ecec940c1c48a2c6c6ed8550dd5c25fe0631c
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-08-07 09:53:13 -0300

    Fixed dropdown menu error

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.scss";
 import Link from "next/link";
 import { ActiveLink } from "../ActiveLink";
 import { AiOutlineMenu } from "react-icons/ai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function Header() {
   const [state, setState] = useState(false);
@@ -18,8 +18,23 @@ export function Header() {
           <AiOutlineMenu size={25} color="white" />
         </button>
 
+        <nav className={styles.menu}>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/about" activeClassName={styles.active}>
+            <a>Sobre</a>
+          </ActiveLink>
+          <ActiveLink href="/projects" activeClassName={styles.active}>
+            <a>Projetos</a>
+          </ActiveLink>
+          <ActiveLink href="/contact" activeClassName={styles.active}>
+            <a>Contato</a>
+          </ActiveLink>
+        </nav>
+
         {state && (
-          <nav>
+          <nav className={styles.dropDown}>
             <ActiveLink href="/" activeClassName={styles.active}>
               <a onClick={() => setState(false)}>Home</a>
             </ActiveLink>
