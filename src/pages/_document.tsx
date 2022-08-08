2d591932c7bbafb265f309add74f6ad88e7215d2
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-08-08 11:04:29 -0300

    fixex-some-paddings-errors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,15 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-         
-          <link rel="shortcut icon" href="./images/user.png" type="image/pngx-icon" />
+          <link
+            rel="shortcut icon"
+            href="./images/user.png"
+            type="image/pngx-icon"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
